db48948c8a125b1bb761b6e61137ece8351df238
Zack Robinson <zwrprofessional@gmail.com>
2017-09-29 12:45:58 -0400

    Made another pass at the RecyclerView

--- app/src/main/java/com/zackeryrobinson/week5codingtest/MainActivity.java
+++ app/src/main/java/com/zackeryrobinson/week5codingtest/MainActivity.java
@@ -2,12 +2,54 @@ package com.zackeryrobinson.week5codingtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+// 1. Create an application to use the Flickr api.  You can use the following endpoint to
+//    get the images http://api.flickr.com/services/feeds/photos_public.gne?tag=kitten&format=json&nojsoncallback=1
+// 2. Screen 1: Should show a recycler view with all the images from the api.
+//    Each ItemView should have a thumbnail of the image and two more fields.
+// 3. On Clicking long press on each item a dialog box with two options will appear.
+//    “Show full image” and “Show small image”. You can add options to the array in an xml
+//    and pass to the dialog.
+//    https://developer.android.com/guide/topics/ui/dialogs.html#AddingAList
+// 4. “Show full image” will start a new activity and cover the whole screen with the
+//     passed picture.
+// 5. “Show small image” will show the image in a dialog in the same activity.
 public class MainActivity extends AppCompatActivity {
-
+    RecyclerView mRecyclerView;
+    RecyclerViewAdapter adapter;
+    RecyclerView.LayoutManager mLayoutManager;
+    private static final int DATASET_COUNT = 60;
+    private enum LayoutManagerType {
+        LINEAR_LAYOUT_MANAGER
+    }
+    protected LayoutManagerType mCurrentLayoutManagerType;
+    protected String[] mDataset;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initDataset();
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        mLayoutManager = new LinearLayoutManager(this);
+        adapter = new RecyclerViewAdapter(mDataset, null, null);
+        // Set CustomAdapter as the adapter for RecyclerView.
+        mRecyclerView.setAdapter(adapter);
+        int scrollPosition = 0;
+        mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.scrollToPosition(scrollPosition);
+
     }
+
+
+    private void initDataset() {
+        mDataset = new String[DATASET_COUNT];
+        for (int i = 0; i < DATASET_COUNT; i++) {
+            mDataset[i] = "This is element #" + i;
+        }
+    }
+
+
 }
--- app/src/main/java/com/zackeryrobinson/week5codingtest/RetrofitHelper.java
+++ app/src/main/java/com/zackeryrobinson/week5codingtest/RetrofitHelper.java
@@ -1,7 +1,15 @@
 package com.zackeryrobinson.week5codingtest;
 
+import com.zackeryrobinson.week5codingtest.model.FlickR;
+
+import java.security.acl.Owner;
+import java.util.List;
+
+import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 /**
  * Created by Zack on 9/29/2017.
@@ -23,5 +31,30 @@ public class RetrofitHelper {
         return retrofit;
     }
 
+    //create a static method to use the interface verbs
+    public static Call<List<FlickR>> createCall(String user) {
+        Retrofit retrofit = create();
+        ApiService apiService = retrofit.create(ApiService.class);
+        return apiService.getFlickRItems(user);
+    }
+
+    public static Call<Owner> createCallUser(String user) {
+        Retrofit retrofit = create();
+        ApiService apiService = retrofit.create(ApiService.class);
+        return apiService.getFlickRMedia(user);
+    }
+
+
+
+    //create an interface to have all the paths and verbs for the REST api to use
+    interface ApiService {
+
+        @GET("users/{user}/repos")
+        Call<List<FlickR>> getFlickRItems(@Path("user") String user);
+
+        @GET("users/{user}")
+        Call<Owner> getFlickRMedia(@Path("user") String user);
+
+    }
 
 }
